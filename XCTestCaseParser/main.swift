9e66588bac94504c6748c17e882240c5702b8a36
Tomas Camin <tomas.camin@scmitaly.it>
2017-12-14 16:45:07 +0100

    Properly wait on completion block to end

--- XCTestCaseParser/main.swift
+++ XCTestCaseParser/main.swift
@@ -35,19 +35,27 @@ oq.maxConcurrentOperationCount = OperationQueue.defaultMaxConcurrentOperationCou
 
 var result = [String]()
 
+var semaphores = [DispatchSemaphore]()
 for file in files {
     let parseOperation = ParseOperation(sourcePath: file)
+    
+    let sem = DispatchSemaphore(value: 0)
+    semaphores.append(sem)
     parseOperation.completionBlock = { [unowned parseOperation] in
         synchQueue.sync {
             for (k, v) in parseOperation.result {
                 result.append(contentsOf: v.map { k + "/" + $0})
             }
         }
+        sem.signal()
     }
     oq.addOperation(parseOperation)
 }
 
 oq.waitUntilAllOperationsAreFinished()
+for semaphore in semaphores {
+    semaphore.wait()
+}
 
 if let objectData = try? JSONSerialization.data(withJSONObject: result, options: JSONSerialization.WritingOptions(rawValue: 0)),
     let objectString = String(data: objectData, encoding: .utf8) {
